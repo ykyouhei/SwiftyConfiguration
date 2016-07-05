add12a306fbfd93f86cb4a4cd0184351f38c4bc4
ykyouhei <kyouhei.lab@gmail.com>
2016-07-05 22:24:41 +0900

    Initial Commit

---
+++ SwiftyConfiguration/SwiftyConfiguration.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyConfiguration.h
+//  SwiftyConfiguration
+//
+//  Created by kyo__hei on 2016/07/05.
+//  Copyright © 2016年 kyo__hei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyConfiguration.
+FOUNDATION_EXPORT double SwiftyConfigurationVersionNumber;
+
+//! Project version string for SwiftyConfiguration.
+FOUNDATION_EXPORT const unsigned char SwiftyConfigurationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyConfiguration/PublicHeader.h>
+
+
